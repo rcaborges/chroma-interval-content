2d260f5becc1026c3146b836cc0cd72557a03783
Rodrigo <rcaborges@gmail.com>
2019-06-24 22:11:18 +0300

    remove unused methods and small adjusts

--- classify.py
+++ classify.py
@@ -3,38 +3,26 @@ import json
 
 import numpy as np
 import simplejson
-from sklearn.metrics import jaccard_similarity_score
 import time
 import matplotlib.pyplot as plt
 import pandas as pd
-from scipy.cluster.vq import vq, kmeans, whiten
-import scipy
 
 from xgboost import XGBClassifier
 from sklearn.metrics import classification_report
 from sklearn.model_selection import ParameterGrid
 from sklearn import preprocessing
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
+from sklearn import metrics
+from sklearn.model_selection import cross_val_score
 
 data = json.load(open("data.json"))
 
-def plot_histogram(anos):
-    fig,ax = plt.subplots()
-    plt.bar( range(12), anos)
-    labels = ['-5','-4','-3','-2','-1','1','2','3','4','5','6']
-    indexes = np.arange(len(anos))
-    plt.xticks(indexes, labels)
-    #fig.savefig('histogram.png', bbox_inches='tight')
-    plt.show()
-
 def kmeans_cluster(songs,i_max):
-    from sklearn.cluster import KMeans
-    from sklearn.metrics import silhouette_samples, silhouette_score
-
     clusterer = KMeans(n_clusters=i_max, random_state=10)
     cluster_labels = clusterer.fit_predict(songs)
     s_avg = silhouette_score(songs, cluster_labels)
     #print(s_avg)
-
     return cluster_labels, clusterer.cluster_centers_, clusterer,s_avg
 
 def codewords(data,method,codes):
@@ -55,16 +43,6 @@ def codewords(data,method,codes):
     matrix = matrix[:,1:].T
     print(matrix.shape)
     
-    ### CODEWORDS
-#    codes = 2
-#    avgmax = 0
-#    for i in np.arange(2,100,1):
-#        labels,centroids,kmeans,s_avg = kmeans_cluster(matrix,i)
-#        print(i,s_avg,codes)
-#        if s_avg > avgmax: 
-#            avgmax = s_avg
-#            codes = i
-#    print("MAX: ",codes,avgmax)
     labels,centroids,kmeans,s_avg = kmeans_cluster(matrix,codes)
     pop_cent = []
     #pop_cent.append(len(ds))
@@ -109,9 +87,7 @@ def codewords(data,method,codes):
 
     data_t = np.column_stack((cic,label_final))
     data_final = []
-    #grs = list(letter_counts)
-    grs = ["Rock","Slow","Dance","Country"]
-    #grs = ["Rock","Slow"]
+    grs = ["Rock","Slow","Dance"]
     for itt in data_t:
         if itt[-1] in grs: data_final.append(itt)
      
@@ -127,30 +103,9 @@ def codewords(data,method,codes):
     return np.array(final_final)
 
 def classify(data_final):
-    #split = int(0.8*data_final.shape[0])
-    #
-    #X_train = data_final[:split,:-1].astype(np.float)
-    #X_test = data_final[split:,:-1].astype(np.float)
-    #y_train = data_final[:split,-1].astype(int)
-    #y_test = data_final[split:,-1].astype(int)
-    #
     NUM_THREADS = 5
-    #print('Training model...')
-    ##new_params = {'gamma': 0, 'learning_rate': 0.2, 'max_depth': 15, 'n_estimetors': 100}
     new_params = {'gamma': 0, 'learning_rate': 0.2, 'max_depth': 15}
     xgb = XGBClassifier(**new_params, nthread=NUM_THREADS, seed=1, missing=np.nan)
-    #xgb = XGBClassifier()
-    #xgb.fit(X_train, y_train)
-    #print('Predicting...')
-    #y_pred = xgb.predict(X_test)
-    #report = classification_report(y_test, y_pred)
-    #print(report)
-    #print('Predicting...')
-    #y_pred = xgb.predict(X_train)
-    #report = classification_report(y_train, y_pred)
-    #print(report)
-    from sklearn import metrics
-    from sklearn.model_selection import cross_val_score
 
     N_FOLD = 5
     scores = cross_val_score(xgb, data_final[:,:-1].astype(float), data_final[:,-1], cv=N_FOLD, scoring='f1_weighted')
@@ -160,40 +115,10 @@ def classify(data_final):
     scores = cross_val_score(xgb, data_final[:,:-1].astype(float), data_final[:,-1], cv=N_FOLD, scoring='f1_macro')
     print("Fscore Macro: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
-def plot_feature(data):
-    from sklearn.decomposition import PCA
-    from sklearn.manifold import TSNE
-    import matplotlib.pyplot as plt
-    RS = 20150101
-    pca = PCA(n_components=7)
-    #X_t = pca.fit(data[:,:-1].astype(np.float)).transform(data[:,:-1].astype(np.float))
-    X_t = TSNE(random_state=RS).fit_transform(data[:,:-1].astype(np.float))
-    #print(np.sum(pca.explained_variance_ratio_)) 
-
-    from sklearn import preprocessing
-    le = preprocessing.LabelEncoder()
-    le.fit(data[:,-1])
-    labels = le.transform(data[:,-1])
-    plt.figure(figsize=(10, 8))
-    plt.scatter(X_t[:,0], X_t[:,1],c=labels,cmap='prism')  # plot points with cluster dependent colors
-    #plt.scatter(X_t[:,0], X_t[:,1],cmap='prism')  # plot points with cluster dependent colors
-    i=0
-    for txt in data[:,-1]:
-        plt.annotate(str(txt),(X_t[i,0],X_t[i,1]))
-        i=i+1
-    plt.show()
-
 
 codeMCIC = codewords(data,"CIC",4)
 codeMDCF = codewords(data,"DCF",5)
 
-#arr = np.arange(codeMCIC.shape[0])
-#np.random.shuffle(arr)
-#codeMCIC = codeMCIC[arr,:]
-#codeMDCF = codeMDCF[arr,:]
-
 classify(codeMCIC)
 classify(codeMDCF)
 
-#plot_feature(codeMCIC)
-#plot_feature(codeMDCF)
